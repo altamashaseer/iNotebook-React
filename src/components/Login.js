24eea39051a2b10ce0558bff86fcd5f03ba75b5d
Altamash <altamashaseer9@gmail.com>
2022-10-06 00:10:14 +0530

    Login fixes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
+// import { useHistory } from 'react-router'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const Login = () => {
-    
+
     const [credentials, setCredentials] = useState({ email: '', password: '' })
-    let history = useHistory();
+    // let history = useHistory();
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -17,32 +20,36 @@ const Login = () => {
         });
         const json = await response.json();
         if (json.success) {
-                //redirect and save auth token
+            //redirect and save auth token
             localStorage.setItem('token', json.authtoken)
-            history.push("/")
+            navigate("/")
         }
         else {
             alert('Invalid Credentials')
         }
     }
 
-}
-return (
-    <div>
-        <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-                <label for="email" className="form-label">Email address</label>
-                <input type="email" className="form-control" onChange={onChange} id="email" name='email' aria-describedby="emailHelp" />
-                <div id="emailHelp" className="form-text" >We'll never share your email with anyone else.</div>
-            </div>
-            <div className="mb-3">
-                <label for="password" className="form-label">Password</label>
-                <input type="password" className="form-control" onChange={onChange} id="password" />
-            </div>
-            <button type="submit" className="btn btn-primary" >Submit</button>
-        </form>
-    </div>
-)
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
 
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" onChange={onChange} id="email" name='email' aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text" >We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" onChange={onChange} id="password" />
+                </div>
+                <button type="submit" className="btn btn-primary" >Submit</button>
+            </form>
+        </div>
+    )
+
+}
 
 export default Login
