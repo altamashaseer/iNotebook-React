5267d2190af8009415e231873524dce26cee69eb
Altamash <altamashaseer9@gmail.com>
2022-11-16 02:07:10 +0530

    Fixes, About bg

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ import '../About.css'
 const About = () => {
   return (
     <div>
+      <div className="back"></div>
       <section className="about-section">
         <div className="container">
           <div className="row">
@@ -18,15 +19,15 @@ const About = () => {
                   We are here to serve you next level tutorial that currently in trend to match you with your expertise. Css3 transition is a learning website. where you can find many good quality content related to web development and tutorials about plugins. here we are using html, html5, css, css3, jquery & javascript along with inspirational UI design layout by professionals by using Photoshop and adobe allustrator.
                 </div>
                 <div className="btn-box">
-                  <a href="#" className="theme-btn btn-style-one">Contact Us</a>
+                  <a href="#$" className="theme-btn btn-style-one">Contact Us</a>
                 </div>
               </div>
             </div>
 
-            {/* <!-- Image Column --> */}
+            {/* Image Column  */}
             <div className="image-column col-lg-6 col-md-12 col-sm-12">
               <div className="inner-column wow fadeInLeft">
-                <figure className="image-1"><a href="#" className="lightbox-image" data-fancybox="images"><img title="Rahul Kumar Yadav" src="https://i.ibb.co/QP6Nmpf/image-1-about.jpg" alt="" /></a></figure>
+                <figure className="image"><img title="Rahul Kumar Yadav" src="https://i.ibb.co/QP6Nmpf/image-1-about.jpg" alt=""/></figure>
               </div>
             </div>
           </div>
